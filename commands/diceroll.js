3948defebf9cb8cd8c8661aca32eec70700baae1
RedTea <myadiuio@gmail.com>
2020-12-19 19:25:30 +0900

    Procfile

---
+++ commands/diceroll.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+
+module.exports = {
+	name: 'diceroll',
+	description: 'Roll a dice.',
+	execute(interaction) {
+        var facts = ["1", "2", "3", "4", "5", "6"]
+        var fact = Math.floor(Math.random() * facts.length);     
+        const diceembed = new Discord.MessageEmbed()
+            .setColor('#9c51b6')
+            .setTitle('Coin flip')
+            .setDescription( 'Result: ' + facts[fact] )
+            .setTimestamp()
+		client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+			type: 4,
+			data: {
+					embeds: [diceembed]
+				}
+			}
+		})
+	},
+};
