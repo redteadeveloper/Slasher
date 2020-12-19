b3ad328a414dcff40d40e20883027394bf041ac1
RedTea <myadiuio@gmail.com>
2020-12-19 20:13:04 +0900

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -10,6 +10,23 @@ module.exports = {
 		}
 	],
 	execute(interaction) {
+        var answers = ["Yes.",
+                 "Ask me later.", 
+                 "No.", 
+                 "I don't know",
+                 "Of course.",
+                 "Never.", 
+                 "Maybe.",
+                 "Hmm...",
+                 "Excuse me?"];
+
+        var answer = Math.floor(Math.random() * answers.length);
+ 
+        const ballembed = new Discord.MessageEmbed()
+            .setColor('#9c51b6')
+            .setTitle('**8ball**')
+            .setDescription( '**Your question**: ' + interaction.data.options[0].value + '\n**My answer**: ' + answers[answer] )
+            .setTimestamp() 
 		client.api.interactions(interaction.id, interaction.token).callback.post({data: {
 			type: 4,
 			data: {
