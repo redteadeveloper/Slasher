8c4a56aa52140c11e1d6c9775ed32c47b7d8b8e4
unknown <myadiuio@gmail.com>
2020-12-23 18:17:19 +0900

    Create wiki.js

---
+++ commands/wiki.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
+
+module.exports = {
+	name: 'eval',
+	description: 'Executes JavaScript code.',
+	commandOptions: [
+		{
+			type: 3,
+            name: "code",
+            description: "Code to execute",
+            required: true
+		}
+    ],
+	global: false,
+	async execute(interaction) {
+
+        const args1 = interaction.data.options[0].value.split(" ")
+        const search = args1.join('_');
+
+        const searchword = encodeURI(search)
+
+        const res = await fetch("https://en.wikipedia.org/api/rest_v1/page/summary/" + searchword);
+        const data = await res.json();
+
+        const title = data.title;
+        const text = data.extract || "Couldn't retrieve any result. Try searching case sensitively.";
+
+        let thumbnail = data.originalimage ? data.originalimage.source : null 
+        let url = data.content_urls ? data.content_urls.desktop.page : null
+
+        const embed = new Discord.MessageEmbed()
+            .setColor(`#009900`)
+            .setTitle(title)
+            .setURL(url)
+            .setThumbnail(thumbnail)
+            .setDescription(text)
+            .setFooter("Powered by Wikipedia", "https://i.ibb.co/VWvCzg1/wikipedia.png")
+
+            client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+                type: 4,
+                data: {
+                        embeds: [embed]
+                    }
+                }
+            })
+	},
+};
